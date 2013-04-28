96cea4a759614247742e64e463845dad7766f695
Martin Vilcans <martin@librador.com>
2013-04-28 16:56:59 +0200

    Threshold on stairs

--- content/js/collision.js
+++ content/js/collision.js
@@ -1,4 +1,4 @@
-//Collision mesh data!
+//Collision mesh data! 32 polys
 window.addCollisionData = function(b2Vec2, world, bodyDef, fixDef) {
   fixDef.shape.SetAsArray([
     new b2Vec2(4.100000381469727, 0.2999999523162842), new b2Vec2(2.0, 0.2999999523162842), new b2Vec2(2.0, 0.20000004768371582), new b2Vec2(4.100000381469727, 0.20000004768371582)
@@ -6,14 +6,6 @@ window.addCollisionData = function(b2Vec2, world, bodyDef, fixDef) {
   world.CreateBody(bodyDef).CreateFixture(fixDef);
   fixDef.shape.SetAsArray([
     new b2Vec2(-8.5, 5.5), new b2Vec2(-9.5, 5.5), new b2Vec2(-9.5, -10.0), new b2Vec2(-8.5, -10.0)
-  ]);
-  world.CreateBody(bodyDef).CreateFixture(fixDef);
-  fixDef.shape.SetAsArray([
-    new b2Vec2(-7.800000190734863, 7.0), new b2Vec2(-9.5, 7.0), new b2Vec2(-9.5, 5.5), new b2Vec2(-7.800000190734863, 5.5)
-  ]);
-  world.CreateBody(bodyDef).CreateFixture(fixDef);
-  fixDef.shape.SetAsArray([
-    new b2Vec2(-7.599999904632568, -10.0), new b2Vec2(-9.5, -10.0), new b2Vec2(-9.5, -11.0), new b2Vec2(-7.599999904632568, -11.0)
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
   fixDef.shape.SetAsArray([
@@ -29,15 +21,7 @@ window.addCollisionData = function(b2Vec2, world, bodyDef, fixDef) {
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
   fixDef.shape.SetAsArray([
-    new b2Vec2(10.0, 8.0), new b2Vec2(9.0, 8.0), new b2Vec2(9.0, -10.0), new b2Vec2(10.0, -10.0)
-  ]);
-  world.CreateBody(bodyDef).CreateFixture(fixDef);
-  fixDef.shape.SetAsArray([
     new b2Vec2(9.0, -1.7999999523162842), new b2Vec2(5.800000190734863, -1.7999999523162842), new b2Vec2(5.800000190734863, -2.299999952316284), new b2Vec2(9.0, -2.299999952316284)
-  ]);
-  world.CreateBody(bodyDef).CreateFixture(fixDef);
-  fixDef.shape.SetAsArray([
-    new b2Vec2(6.19999885559082, 0.8000000715255737), new b2Vec2(5.800000190734863, 0.8000000715255737), new b2Vec2(5.800000190734863, -1.7999999523162842), new b2Vec2(6.19999885559082, -1.7999999523162842)
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
   fixDef.shape.SetAsArray([
@@ -57,10 +41,6 @@ window.addCollisionData = function(b2Vec2, world, bodyDef, fixDef) {
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
   fixDef.shape.SetAsArray([
-    new b2Vec2(8.0, 6.0), new b2Vec2(6.199999809265137, 6.0), new b2Vec2(6.199999809265137, 5.599999904632568), new b2Vec2(8.0, 5.599999904632568)
-  ]);
-  world.CreateBody(bodyDef).CreateFixture(fixDef);
-  fixDef.shape.SetAsArray([
     new b2Vec2(6.200000762939453, 6.0), new b2Vec2(5.800000190734863, 6.0), new b2Vec2(5.800000190734863, 2.0), new b2Vec2(6.200000762939453, 2.0)
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
@@ -69,19 +49,7 @@ window.addCollisionData = function(b2Vec2, world, bodyDef, fixDef) {
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
   fixDef.shape.SetAsArray([
-    new b2Vec2(2.1000003814697266, 1.8000001907348633), new b2Vec2(2.0, 1.8000001907348633), new b2Vec2(2.0, 0.30000007152557373), new b2Vec2(2.1000003814697266, 0.30000007152557373)
-  ]);
-  world.CreateBody(bodyDef).CreateFixture(fixDef);
-  fixDef.shape.SetAsArray([
-    new b2Vec2(2.1000003814697266, 1.9000000953674316), new b2Vec2(1.0, 1.9000000953674316), new b2Vec2(1.0, 1.7999999523162842), new b2Vec2(2.1000003814697266, 1.7999999523162842)
-  ]);
-  world.CreateBody(bodyDef).CreateFixture(fixDef);
-  fixDef.shape.SetAsArray([
     new b2Vec2(1.0, 2.299999952316284), new b2Vec2(0.0, 2.299999952316284), new b2Vec2(0.0, -2.0), new b2Vec2(1.0, -2.0)
-  ]);
-  world.CreateBody(bodyDef).CreateFixture(fixDef);
-  fixDef.shape.SetAsArray([
-    new b2Vec2(-3.6999998092651367, 0.10000014305114746), new b2Vec2(-3.8999996185302734, 0.10000014305114746), new b2Vec2(-3.8999996185302734, -3.5999999046325684), new b2Vec2(-3.6999998092651367, -3.5999999046325684)
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
   fixDef.shape.SetAsArray([
@@ -97,7 +65,51 @@ window.addCollisionData = function(b2Vec2, world, bodyDef, fixDef) {
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
   fixDef.shape.SetAsArray([
-    new b2Vec2(-4.5, 0.5), new b2Vec2(-4.900000095367432, 0.5), new b2Vec2(-4.900000095367432, -0.10000002384185791), new b2Vec2(-4.5, -0.10000002384185791)
+    new b2Vec2(-3.700000762939453, 5.799999713897705), new b2Vec2(-3.90000057220459, 5.799999713897705), new b2Vec2(-3.90000057220459, 5.5), new b2Vec2(-3.700000762939453, 5.5)
+  ]);
+  world.CreateBody(bodyDef).CreateFixture(fixDef);
+  fixDef.shape.SetAsArray([
+    new b2Vec2(-6.0, 0.5), new b2Vec2(-8.5, 0.5), new b2Vec2(-8.5, 0.30000007152557373), new b2Vec2(-6.0, 0.30000007152557373)
+  ]);
+  world.CreateBody(bodyDef).CreateFixture(fixDef);
+  fixDef.shape.SetAsArray([
+    new b2Vec2(-7.300000190734863, -1.4000000953674316), new b2Vec2(-8.5, -1.4000000953674316), new b2Vec2(-8.5, -1.600000023841858), new b2Vec2(-7.300000190734863, -1.600000023841858)
+  ]);
+  world.CreateBody(bodyDef).CreateFixture(fixDef);
+  fixDef.shape.SetAsArray([
+    new b2Vec2(-7.800000190734863, 7.0), new b2Vec2(-9.5, 7.0), new b2Vec2(-9.5, 5.5), new b2Vec2(-7.800000190734863, 5.5)
+  ]);
+  world.CreateBody(bodyDef).CreateFixture(fixDef);
+  fixDef.shape.SetAsArray([
+    new b2Vec2(-7.599999904632568, -10.0), new b2Vec2(-9.5, -10.0), new b2Vec2(-9.5, -11.0), new b2Vec2(-7.599999904632568, -11.0)
+  ]);
+  world.CreateBody(bodyDef).CreateFixture(fixDef);
+  fixDef.shape.SetAsArray([
+    new b2Vec2(10.0, 8.0), new b2Vec2(9.0, 8.0), new b2Vec2(9.0, -10.0), new b2Vec2(10.0, -10.0)
+  ]);
+  world.CreateBody(bodyDef).CreateFixture(fixDef);
+  fixDef.shape.SetAsArray([
+    new b2Vec2(6.19999885559082, 0.8000000715255737), new b2Vec2(5.800000190734863, 0.8000000715255737), new b2Vec2(5.800000190734863, -1.7999999523162842), new b2Vec2(6.19999885559082, -1.7999999523162842)
+  ]);
+  world.CreateBody(bodyDef).CreateFixture(fixDef);
+  fixDef.shape.SetAsArray([
+    new b2Vec2(8.0, 6.0), new b2Vec2(6.200000762939453, 6.0), new b2Vec2(6.199999809265137, 5.599999904632568), new b2Vec2(8.0, 5.599999904632568)
+  ]);
+  world.CreateBody(bodyDef).CreateFixture(fixDef);
+  fixDef.shape.SetAsArray([
+    new b2Vec2(2.1000003814697266, 1.8000001907348633), new b2Vec2(2.0, 1.8000001907348633), new b2Vec2(2.0, 0.2999999523162842), new b2Vec2(2.1000003814697266, 0.30000007152557373)
+  ]);
+  world.CreateBody(bodyDef).CreateFixture(fixDef);
+  fixDef.shape.SetAsArray([
+    new b2Vec2(2.1000003814697266, 1.9000000953674316), new b2Vec2(1.0, 1.9000000953674316), new b2Vec2(1.0, 1.7999999523162842), new b2Vec2(2.1000003814697266, 1.8000001907348633)
+  ]);
+  world.CreateBody(bodyDef).CreateFixture(fixDef);
+  fixDef.shape.SetAsArray([
+    new b2Vec2(-3.6999998092651367, 0.10000014305114746), new b2Vec2(-3.9000000953674316, 0.09999990463256836), new b2Vec2(-3.8999996185302734, -3.5999999046325684), new b2Vec2(-3.6999998092651367, -3.5999999046325684)
+  ]);
+  world.CreateBody(bodyDef).CreateFixture(fixDef);
+  fixDef.shape.SetAsArray([
+    new b2Vec2(-4.5, 0.5), new b2Vec2(-4.900000095367432, 0.5), new b2Vec2(-4.900000095367432, -0.10000002384185791), new b2Vec2(-4.5, -0.09999996423721313)
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
   fixDef.shape.SetAsArray([
@@ -109,19 +121,11 @@ window.addCollisionData = function(b2Vec2, world, bodyDef, fixDef) {
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
   fixDef.shape.SetAsArray([
-    new b2Vec2(-3.700000286102295, 4.399999618530273), new b2Vec2(-3.9000000953674316, 4.399999618530273), new b2Vec2(-3.9000000953674316, 4.199999809265137), new b2Vec2(-3.700000286102295, 4.199999809265137)
+    new b2Vec2(-3.700000286102295, 4.399999618530273), new b2Vec2(-3.9000000953674316, 4.399999618530273), new b2Vec2(-3.9000000953674316, 4.199999809265137), new b2Vec2(-3.6999998092651367, 4.199999809265137)
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
   fixDef.shape.SetAsArray([
-    new b2Vec2(-3.700000762939453, 5.799999713897705), new b2Vec2(-3.90000057220459, 5.799999713897705), new b2Vec2(-3.90000057220459, 5.5), new b2Vec2(-3.700000762939453, 5.5)
-  ]);
-  world.CreateBody(bodyDef).CreateFixture(fixDef);
-  fixDef.shape.SetAsArray([
-    new b2Vec2(-6.0, 0.5), new b2Vec2(-8.5, 0.5), new b2Vec2(-8.5, 0.30000007152557373), new b2Vec2(-6.0, 0.30000007152557373)
-  ]);
-  world.CreateBody(bodyDef).CreateFixture(fixDef);
-  fixDef.shape.SetAsArray([
-    new b2Vec2(-7.300000190734863, -1.4000000953674316), new b2Vec2(-8.5, -1.4000000953674316), new b2Vec2(-8.5, -1.600000023841858), new b2Vec2(-7.300000190734863, -1.600000023841858)
+    new b2Vec2(3.3999996185302734, -0.8999999761581421), new b2Vec2(4.100000381469727, -0.8999999761581421), new b2Vec2(4.100000381469727, 0.20000004768371582), new b2Vec2(3.3999996185302734, 0.20000004768371582)
   ]);
   world.CreateBody(bodyDef).CreateFixture(fixDef);
 }
